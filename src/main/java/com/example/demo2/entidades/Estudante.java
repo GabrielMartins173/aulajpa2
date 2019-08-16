bf2b6b1ec35fe982e0b697e88f2f908561369cc6
Gabriel Martins <gabrielmartins173@gmail.com>
2019-08-16 15:40:01 -0300

    Adicionadas validações de dados de entrada.

--- src/main/java/com/example/demo2/entidades/Estudante.java
+++ src/main/java/com/example/demo2/entidades/Estudante.java
@@ -4,15 +4,25 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.*;
 
 @Entity
 @Table(name= "estudantes")
 public class Estudante {
 
     @Id
+    @NotBlank
+    @NotEmpty
     private String matricula;
+
+
+    @NotBlank(message = "Nome nao pode estar em branco.")
+    @NotEmpty(message = "Nome nao pode estar vazio ou nulo.")
+    @Size(min= 2, max = 255, message = "Nome deve ter entre 2 e 255 caracteres.")
     @Column(nullable = false)
     private String nome;
+
+    @Positive(message = "Idade deve ser um numero maior que zero.")
     @Column(nullable = false)
     private int idade;
 
